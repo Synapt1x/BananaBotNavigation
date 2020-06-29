f94dab049ef0e583ca52b7047d343547f31bdc99
Synapt1x <chriscadonic@gmail.com>
2020-06-28 21:54:40 -0500

    Add framework for Q

--- navigation/agent.py
+++ navigation/agent.py
@@ -9,10 +9,8 @@ For Deep Reinforcement Learning Nanodegree offered by Udacity.
 """
 
 
-import os
-import argparse
-
 import numpy as np
+from navigation.q import Q
 
 
 class MainAgent:
@@ -21,23 +19,40 @@ class MainAgent:
     through the pertinent problem.
     """
 
-    def __init__(self, alg, state_size, action_size, **kwargs):
+    def __init__(self, alg, state_size, action_size, seed=13, **kwargs):
         self.alg = alg
         self.state_size = state_size
         self.action_size = action_size
+        self.seed = seed
+
+        np.random.seed(seed)
+
+        self.epsilon = kwargs.get('epsilon', 0.9)
+        self.epslion_decay = kwargs.get('epsilon_decay', 0.999)
+        self.epsilon_min = kwargs.get('epsilon_min', 0.1)
+
+        self.q = Q(alg, self.state_size, self.action_size)
+
+    def _select_random_a(self):
+        """
+        Select a random action.
+        """
+        return np.random.randint(self.action_size)
 
     def get_action(self, state):
         """
         Extract the action intended by the agent based on the selection
         criteria.
         """
-        #TODO: currently completely random
-        action = np.random.randint(self.action_size)
-
-        return action
+        if self.alg == 'random':
+            return self._select_random_a()
+        elif self.alg == 'q':
+            rand_val = np.random.rand()
+            if rand_val < self.epsilon:
+                return self._select_random_a()
+            return self.q.get_action(state)
 
     def learn(self):
         """
-
         """
         pass
---
+++ navigation/q.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Custom RL agent for learning how to navigate through the Unity-ML environment
+provided in the project.
+
+Author: Chris Cadonic
+
+For Deep Reinforcement Learning Nanodegree offered by Udacity.
+"""
+
+
+import numpy as np
+
+
+class Q:
+    """
+    This code contains the functionality for initializing, deciding on actions,
+    and updating the underlying Q-function representation.
+    """
+
+    def __init__(self, alg, state_size, action_size, **kwargs):
+        self.alg = alg
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.q = self._init_q()
+
+    def _init_q(self):
+        """
+        Initialize the representation of the Q-function.
+        """
+        if self.alg == 'random':
+            return None
+        elif self.alg == 'q':
+            return np.random.rand(shape=(self.state_size, self.action_size))
+
+    def s_a_value(self, state, action):
+        """
+        """
+        if self.alg == 'q':
+            return q[state, action]
+
+    def get_action(self, state):
+        """
+        """
+        if self.alg == 'q':
+            return np.argmax(q[state])
+
+    def update_value(self, state, action, new_val):
+        """
+        """
+        if self.alg == 'q':
+            self.q[state, action] = new_val
+
