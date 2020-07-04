a4cab1a16200081cf64fe21cb0589c6183957e9a
Synapt1x <chriscadonic@gmail.com>
2020-07-03 20:03:02 -0500

    Try DDQN by switching target and q in eval/select

--- navigation/agent.py
+++ navigation/agent.py
@@ -133,8 +133,8 @@ class MainAgent:
         else:
             # get target q for next state (on max) and current estimate
             if 'ddqn' in self.alg.lower():
-                max_action = self.q.get_action(next_state)
-                target_q_vals = self.target_q.get_value(next_state, max_action)
+                max_action = self.target_q.get_action(next_state)
+                target_q_vals = self.q.get_value(next_state, max_action)
             else:
                 target_q_vals = self.target_q.get_value(next_state)
             curr_q_est = self.q.get_value(state, action)
